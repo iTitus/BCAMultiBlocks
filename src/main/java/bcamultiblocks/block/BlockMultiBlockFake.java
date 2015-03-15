a1b379ef2f68083a8cfdb0e51dc0d6d6d874ca34
big_Xplosion <bigxplosion44@gmail.com>
2015-03-15 20:34:35 +0100

    some more work on wrapping multiblock fake methods from the block

--- src/main/java/bcamultiblocks/block/BlockMultiBlockFake.java
+++ src/main/java/bcamultiblocks/block/BlockMultiBlockFake.java
@@ -3,6 +3,7 @@ package bcamultiblocks.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
@@ -15,6 +16,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 import bcamultiblocks.tile.TileMultiBlockFake;
 
+//TODO: Add needed methods
 public class BlockMultiBlockFake extends BlockMultiBlock {
 
 	public BlockMultiBlockFake(Material material) {
@@ -47,8 +49,49 @@ public class BlockMultiBlockFake extends BlockMultiBlock {
 	}
 
 	@Override
+	public float getBlockHardness(World world, int x, int y, int z) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.getBlockHardness(world, x, y, z) : block.getBlockHardness(world, x, y, z);
+	}
+
+	@Override
+	public float getExplosionResistance(Entity entity, World world, int x, int y, int z, double explosionX, double explosionY, double explosionZ) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.getExplosionResistance(entity, world, x, y, z, explosionX, explosionY, explosionZ) : block.getExplosionResistance(entity, world, x, y, z, explosionX, explosionY, explosionZ);
+	}
+
+	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		return super.onBlockActivated(world, x, y, z, player, side, hitX, hitY, hitZ);
+	}
+
+	@Override
+	public int damageDropped(int meta) {
+		return meta;
+	}
+
+	@Override
+	public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.canConnectRedstone(world, x, y, z, side) : block.canConnectRedstone(world, x, y, z, side);
+	}
+
+	@Override
+	public int isProvidingStrongPower(IBlockAccess world, int x, int y, int z, int i) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.isProvidingStrongPower(world, x, y, z, i) : block.isProvidingStrongPower(world, x, y, z, i);
+	}
+
+	@Override
+	public int isProvidingWeakPower(IBlockAccess world, int x, int y, int z, int i) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.isProvidingWeakPower(world, x, y, z, i) : block.isProvidingWeakPower(world, x, y, z, i);
+	}
+
+	@Override
+	public int getLightValue(IBlockAccess world, int x, int y, int z) {
+		Block block = getOriginalBlock(world, x, y, z);
+		return block == null ? super.getLightValue(world, x, y, z) : block.getLightValue(world, x, y, z);
 	}
 
 	public Block getOriginalBlock(IBlockAccess world, int x, int y, int z) {
